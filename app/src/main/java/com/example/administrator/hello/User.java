d052a163269a81f81d0312f03b3e309ef8effe6c
wangdi <824223161@qq.com>
2016-08-07 15:37:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/hello/User.java
@@ -0,0 +1,30 @@
+package com.example.administrator.hello;
+
+/**
+ * Created by Administrator on 2016/8/7.
+ */
+public class User {
+
+    private String username;
+    private String age;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+
+
+}
